bc470a43f992998d4462e643efac9b9d94784245
immprada <im.mprada@gmail.com>
2025-04-05 17:54:51 -0500

    add job for prcessing the file

--- backend/app/jobs/process_csv_job.rb
+++ backend/app/jobs/process_csv_job.rb
@@ -5,7 +5,19 @@ class ProcessCsvJob < ApplicationJob
     @csv_upload = CsvUpload.find(csv_upload_id)
     raise ActiveRecord::RecordNotFound, 'CsvUpload not found' if csv_upload.blank?
 
-    DataProcessing::ProcessService.new(csv_upload).call!
+    progress_service = CsvProcessings::ProgressService.new(csv_upload)
+    merging_service = DataProcessing::ChunksMergingService.new(
+      csv_upload,
+      progress_service
+    )
+    merging_service.call!
+
+    data_registering_service = DataProcessing::DataRegisteringService.new(
+      csv_upload,
+      merging_service.tempfile,
+      progress_service
+    )
+    data_registering_service.call!
   end
 
   private
