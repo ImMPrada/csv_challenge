8e9e7452d41b43e325513706fbe41c2bece7ce8a
immprada <im.mprada@gmail.com>
2025-04-06 17:58:16 -0500

    paginate products index

--- backend/app/controllers/api/v1/products_controller.rb
+++ backend/app/controllers/api/v1/products_controller.rb
@@ -1,8 +1,16 @@
 module Api
   module V1
     class ProductsController < ApplicationController
+      include Pagy::Backend
+
       def index
-        @products = Product.all
+        @pagy, @products = pagy(scope)
+      end
+
+      private
+
+      def scope
+        @scope ||= Product.all
       end
     end
   end
