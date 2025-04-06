10261123c592fe5ea4b288266672c83011da5d6b
immprada <im.mprada@gmail.com>
2025-04-06 17:38:19 -0500

    show prices in othre exchanges

--- backend/app/views/api/v1/products/index.json.jbuilder
+++ backend/app/views/api/v1/products/index.json.jbuilder
@@ -2,4 +2,8 @@ json.products @products do |product|
   json.name product.name
   json.price product.price
   json.expiration_date product.expiration_date
+  json.foreign_exchange do
+    json.date product.foreign_exchange.date
+    json.rates product.foreign_exchange.rates
+  end
 end
--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -1,5 +1,17 @@
+export interface Rate {
+  currency: string;
+  description: string;
+  rate: number;
+}
+
+export interface ForeignExchange {
+  date: string;
+  rates: Rate[];
+}
+
 export interface Product {
   name: string;
   price: number;
   expiration_date: string;
+  foreign_exchange?: ForeignExchange;
 } 
